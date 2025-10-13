daee7a4dd4df5a100bf389cb87271f8a7713c259
Martijn <martijnvanderwilligen@gmail.com>
2025-10-13 17:22:12 +0200

    if no login exception added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,16 @@
 <x-app-layout>
     <div>hello world</div>
 
-    <h1 class="text-xl font-bold">Hello, {{ auth()->user()->name }}!</h1>
+    @if(auth()->check())
+        <h1> Hello, {{ auth()->user()->name }}! </h1>
 
-    @foreach ($users as $user)
-        <p>This is user {{ $user->id }}</p>
-    @endforeach
+        @foreach ($users as $user)
+            <p> This is user {{ $user->id }} </p>
+        @endforeach
+    @else
+        <h1> Hello, Guest! </h1>
+        <a href="{{ route('login') }}">Login </a>
+    @endif
 
 </x-app-layout>
 
