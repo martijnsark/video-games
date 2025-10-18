1b65d4432642161c7a7f20ebbaceab1c7264a367
Martijn <martijnvanderwilligen@gmail.com>
2025-10-18 13:15:16 +0200

    added logic to create to save user answers

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -6,7 +6,7 @@
             <p class="text-gray-400">create game form</p>
 
             <label for="title" class="text-gray-400">Title:</label>
-            <input type="text" id="title" name="title">
+            <input type="text" id="title" name="title" value="{{ old('title') }}">
             @error('title')
             <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
@@ -14,7 +14,10 @@
             <select name="category_id" id="category_id" class="form-control">
                 <option value=""> select a category </option>
                 @foreach($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    <option value="{{ $category->id }}"
+                        {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                        {{ $category->name }}
+                    </option>
                 @endforeach
             </select>
             @error('category_id')
@@ -22,25 +25,25 @@
             @enderror
 
             <label for="image" class="text-gray-400">Image:</label>
-            <input type="text" id="image" name="image">
+            <input type="text" id="image" name="image" value="{{ old('image') }}">
             @error('image')
             <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
 
             <label for="description" class="text-gray-400">Description:</label>
-            <input type="text" id="description" name="description">
+            <input type="text" id="description" name="description" value="{{ old('description') }}">
             @error('description')
             <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
 
             <label for="price" class="text-gray-400">Price:</label>
-            <input type="number" id="price" name="price">
+            <input type="number" id="price" name="price" value="{{ old('price') }}">
             @error('price')
             <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
 
             <label for="discount" class="text-gray-400">Discount:</label>
-            <input type="number" id="discount" name="discount">
+            <input type="number" id="discount" name="discount" value="{{ old('discount') }}">
             @error('discount')
             <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
