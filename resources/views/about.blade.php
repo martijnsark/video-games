f29bb03fe98514ca741e6d5caa881bb7700f6444
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-10-28 12:30:52 +0100

    quik home and about layout update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <br>
-    <h1 class="text-gray-400">About us</h1>
 
-    <p class="text-gray-400">This website was designed by Martijn a second year student at Hogeschool Rotterdam in the study of Creative Media and Game Technologies.</p>
+    <div class="text-center">
+        <h1 class="text-gray-400">About us</h1>
+
+        <p class="text-gray-400">
+            This website was designed by Martijn a second year student at Hogeschool Rotterdam in the study of Creative Media and Game Technologies.
+        </p>
+    </div>
 </x-app-layout>
 
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
     <div class="text-center">
         <h1 class="text-gray-400">Welcome to Martijns game manager!</h1>
 
-        <p class="text-gray-400">This website was designed for the soul purpose to create, manage, and compare games and their prices and discounts respectively.</p>
+        <p class="text-gray-400">
+            This website was designed for the soul purpose to create, manage, and compare games and their prices and discounts respectively.
+        </p>
 
         @if (auth()->check())
             <div class="text-center">
