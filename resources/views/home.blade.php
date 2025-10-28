fa5d203ce7bc7bee5ecda1870d120d44e8a95ede
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-10-28 14:52:37 +0100

    really small style update for home, it started to annoy me how it looked

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,27 +15,25 @@
     </div>
 
     <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-        <nav class="flex items-center justify-end gap-4">
-
             @if (!auth()->check())
-                <a
-                    href="{{ route('login') }}"
+            <div class="text-center">
+                <button
+                    type="button"
                     class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                >
+                    onclick="window.location='{{ route('login') }}'">
                     Log in
-                </a>
+                </button>
 
                 @if (Route::has('register'))
-                    <a
-                        href="{{ route('register') }}"
+                    <button
+                        type="button"
                         class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                    >
+                        onclick="window.location='{{ route('register') }}'">
                         Register
-                    </a>
+                    </button>
+            </div>
                 @endif
-
             @endif
-        </nav>
     </header>
 
 
